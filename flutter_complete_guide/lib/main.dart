abc13a446e1170e8ba734de501e2a4870f02eec8
Kosuke <maeda.kosuke2507@gmail.com>
2020-07-25 19:56:22 +0900

    Fix MyApp by using Scaffold

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('My First App')),
+        body: Text('this is default text!'),
+      ),
+    );
   }
 }
