8a052d59b9d235276e7e8da3369bc9d9adb02d41
Kosuke <maeda.kosuke2507@gmail.com>
2020-07-26 09:35:17 +0900

    display questions and answers dynamically

--- flutter_complete_guide/lib/answer.dart
+++ flutter_complete_guide/lib/answer.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
+  final String questionText;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.questionText);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +13,7 @@ class Answer extends StatelessWidget {
       child: RaisedButton(
         color: Colors.blue,
         textColor: Colors.white,
-        child: Text('Answer 1'),
+        child: Text(questionText),
         onPressed: selectHandler,
       ),
     );
