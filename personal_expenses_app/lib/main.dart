89ff0cd844ddfbaa36600c242157d7b31a3f0a7e
Kosuke <maeda.kosuke2507@gmail.com>
2020-08-25 22:41:59 +0900

    Add TextField and FlatButton

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -37,7 +37,7 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
@@ -46,6 +46,32 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
                 child: Text('CAHRT!'),
                 elevation: 5,
+              ),
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Title',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Amount',
+                      ),
+                    ),
+                    FlatButton(
+                      child: Text('Add Transaction'),
+                      textColor: Colors.purple,
+                      onPressed: () {},
+                    )
+                  ],
+                ),
               ),
             ),
             Column(
