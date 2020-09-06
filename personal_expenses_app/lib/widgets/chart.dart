faaa5b1159c385257107184a3f8a5e23f8baa4e5
Kosuke <maeda.kosuke2507@gmail.com>
2020-09-06 16:13:00 +0900

    Create Chart Widget

---
+++ personal_expenses_app/lib/widgets/chart.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:personal_expenses_app/models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekday = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      var totalSum = 0.0;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekday.day &&
+            recentTransactions[i].date.month == weekday.month &&
+            recentTransactions[i].date.year == weekday.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      print(DateFormat.E(weekday));
+      print(totalSum);
+
+      return {
+        'day': DateFormat.E(weekday),
+        'amount': totalSum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(
+        20,
+      ),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
