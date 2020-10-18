4926f4c0bb145ac8d00c96768bc9767135d09fad
Kosuke <maeda.kosuke2507@gmail.com>
2020-10-18 18:06:18 +0900

    learn 45. Stateless vs Stateful Components in React - The Complete Guide (incl Hooks, React Router, Redux)

--- react-complete-guide/src/App.tsx
+++ react-complete-guide/src/App.tsx
@@ -1,17 +1,31 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const App: FC = () => (
-  <div className="App">
-    <h1>Hi, I'm React App</h1>
-    <p>this is really wordking!</p>
-    <Person name="Max" age={28} />
-    <Person name="Manu" age={29} >
-      My Hobbies: Racing
-    </Person>
-    <Person name="Stephanie" age={26} />
-  </div>
-)
+const App: FC = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Max', age: 28 },
+    { name: 'Manu', age: 29 },
+    { name: 'Stephanie', age: 26 }
+  ])
+
+  const switchNameHandler = () => {
+    console.log('was clocked!')
+    setPersons([
+      { name: 'Maximum', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 29 }
+    ])
+  }
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm React App</h1>
+      <p>this is really wordking!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      {persons.map((p, id) => <Person key={id} name={p.name} age={p.age} />)}
+    </div>
+  )
+}
 
 export default App;
