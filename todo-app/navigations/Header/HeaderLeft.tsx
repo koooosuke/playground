f9729710c7443d08b5a2070d0855a984aa7f43e5
Kosuke <maeda.kosuke2507@gmail.com>
2020-11-09 07:22:53 +0900

    add bars with font awesome icon

--- todo-app/navigations/Header/HeaderLeft.tsx
+++ todo-app/navigations/Header/HeaderLeft.tsx
@@ -1,18 +1,16 @@
 import React, { FC, useCallback } from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { DrawerActions } from '@react-navigation/routers';
+import Colors from '../../constants/colors';
 
 const HeaderLeft: FC = () => {
   const { dispatch } = useNavigation();
   const onPress = useCallback(() => {
     dispatch(DrawerActions.openDrawer());
   }, [dispatch]);
-  return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>open</Text>
-    </TouchableOpacity>
-  );
+
+  return <FontAwesome.Button name="bars" color={Colors.white} onPress={onPress} />;
 };
 
 export default HeaderLeft;
