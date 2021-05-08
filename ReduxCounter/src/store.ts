76b11d52b6afb22503e8ca74ed3e34368ad22f49
Kosuke <maeda.kosuke2507@gmail.com>
2021-05-08 18:28:25 +0900

    wip

--- ReduxCounter/src/store.ts
+++ ReduxCounter/src/store.ts
@@ -1,7 +1,11 @@
-import { createStore } from 'redux'
-
+import { applyMiddleware, createStore, Middleware } from 'redux'
+import thunk from 'redux-thunk'
 import counterReducer, { createInitialState } from './reducers/counter'
 
-const store = createStore(counterReducer, createInitialState())
+const store = createStore(
+  counterReducer,
+  createInitialState(),
+  applyMiddleware(thunk)
+)
 
 export default store
