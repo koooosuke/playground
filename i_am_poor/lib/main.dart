d02a1e24ac5f042d21e8a655ecb1ec23cddd48b5
Kosuke <maeda.kosuke2507@gmail.com>
2021-05-13 01:31:18 +0900

    create poor app

--- i_am_poor/lib/main.dart
+++ i_am_poor/lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I Am Poor'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://icotto.k-img.com/system/press_eye_catches/000/003/602/977703324c299ddf5334cd2824c4bb90bc4c2c24.jpeg?1468399594'),
+        ),
+      ),
+    ),
+  ));
 }
