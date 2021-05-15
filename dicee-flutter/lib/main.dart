23dad9923bde5d3d013aee5f8212abb68457241b
Kosuke <maeda.kosuke2507@gmail.com>
2021-05-15 18:16:05 +0900

    Finish Dice page

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,56 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int _leftDiceNumber = 1;
+  int _rightDiceNumber = 1;
+  final _diceMaxNumber = 6;
+
+  _changeDice() {
+    setState(() {
+      _leftDiceNumber = Random().nextInt(_diceMaxNumber - 1) + 1;
+      _rightDiceNumber = Random().nextInt(_diceMaxNumber - 1) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          DiceButton(
+            diceNumber: _leftDiceNumber,
+            changeDice: _changeDice,
+          ),
+          DiceButton(
+            diceNumber: _rightDiceNumber,
+            changeDice: _changeDice,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DiceButton extends StatelessWidget {
+  const DiceButton({Key key, this.diceNumber, this.changeDice})
+      : super(key: key);
+
+  final int diceNumber;
+  final Function changeDice;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+        onPressed: changeDice,
+        child: Image.asset('images/dice$diceNumber.png'),
+      ),
+    );
   }
 }
