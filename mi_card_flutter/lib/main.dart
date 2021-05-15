918910d6a13632ec6d7732f4efec25e49f501ce0
Kosuke <maeda.kosuke2507@gmail.com>
2021-05-15 14:17:26 +0900

    Create Lesson testing

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -2,11 +2,48 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.yellow,
+                    width: 100.0,
+                    height: 100.0,
+                  ),
+                  Container(
+                    color: Colors.yellow.withOpacity(0.5),
+                    width: 100.0,
+                    height: 100.0,
+                  )
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100.0,
+              ),
+            ],
+          ),
+        ),
+        color: Colors.teal,
+      ),
+    );
+  }
 }
