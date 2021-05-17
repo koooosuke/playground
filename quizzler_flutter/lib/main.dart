ea53945704ac5c9251f48e3b6e5a93b2c91a688e
Kosuke <maeda.kosuke2507@gmail.com>
2021-05-18 08:41:41 +0900

    Add quizzler

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -12,7 +12,15 @@ class Quizzler extends StatefulWidget {
 }
 
 class _QuizzlerState extends State<Quizzler> {
-  final results = [true];
+  final results = [];
+  int questionIndex = 0;
+
+  onAnswer(bool answer) {
+    setState(() {
+      results.add(answer == questions[questionIndex]['result']);
+      questionIndex++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,15 +34,23 @@ class _QuizzlerState extends State<Quizzler> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Quizz(
-                  question: 'Some cats are actually allergic to humans',
+                  question: questions[questionIndex]['question'],
                 ),
                 QuizzButton(
                   color: Colors.green,
                   text: 'True',
+                  onAnswer: () {
+                    print('good');
+                    onAnswer(true);
+                    print(results);
+                  },
                 ),
                 QuizzButton(
                   color: Colors.red,
                   text: 'False',
+                  onAnswer: () {
+                    onAnswer(false);
+                  },
                 ),
                 Row(
                   children: results
@@ -75,10 +91,16 @@ class Quizz extends StatelessWidget {
 }
 
 class QuizzButton extends StatelessWidget {
-  const QuizzButton({Key key, this.color, this.text}) : super(key: key);
+  const QuizzButton({
+    Key key,
+    this.color,
+    this.text,
+    this.onAnswer,
+  }) : super(key: key);
 
   final Color color;
   final String text;
+  final Function onAnswer;
 
   @override
   Widget build(BuildContext context) {
@@ -87,7 +109,9 @@ class QuizzButton extends StatelessWidget {
         margin: EdgeInsets.symmetric(vertical: 15),
         color: color,
         child: TextButton(
-          onPressed: () {},
+          onPressed: () {
+            onAnswer();
+          },
           child: Text(
             text,
             style: TextStyle(color: Colors.white),
@@ -107,9 +131,66 @@ class QuizzResult extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Icon(
-        result ? Icons.check : Icons.ballot,
+        result ? Icons.check : Icons.clear,
         color: result ? Colors.green : Colors.red,
       ),
     );
   }
 }
+
+final questions = [
+  {
+    'question': 'Some cats are actually allergic to humans',
+    'result': true,
+  },
+  {
+    'question': 'Approximately one quarter of human bones are in the feet.',
+    'result': true,
+  },
+  {
+    'question': 'A slug\'s blood is green.',
+    'result': true,
+  },
+  {
+    'question': 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
+    'result': true,
+  },
+  {
+    'question': 'It is illegal to pee in the Ocean in Portugal.',
+    'result': true,
+  },
+  {
+    'question':
+        'No piece of square dry paper can be folded in half more than 7 times.',
+    'result': false,
+  },
+  {
+    'question':
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+    'result': true,
+  },
+  {
+    'question':
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+    'result': false,
+  },
+  {
+    'question':
+        'The total surface area of two human lungs is approximately 70 square metres.',
+    'result': true,
+  },
+  {
+    'question': 'Google was originally called \"Backrub\".',
+    'result': true,
+  },
+  {
+    'question':
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+    'result': true,
+  },
+  {
+    'question':
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+    'result': true,
+  }
+];
