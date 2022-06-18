b5e12846d05d634f0f58aca06375fe4ba1c0ac46
koooosuke <maeda.kosuke2507@gmail.com>
2022-06-18 11:17:53 +0900

    addnum

---
+++ asm/addnum.s
@@ -0,0 +1,39 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 12, 0	sdk_version 12, 1
+	.globl	_AddNum                         ; -- Begin function AddNum
+	.p2align	2
+_AddNum:                                ; @AddNum
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	str	w0, [sp, #12]
+	str	w1, [sp, #8]
+	ldr	w8, [sp, #12]
+	ldr	w9, [sp, #8]
+	add	w0, w8, w9
+	add	sp, sp, #16
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.globl	_MyFunc                         ; -- Begin function MyFunc
+	.p2align	2
+_MyFunc:                                ; @MyFunc
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #32
+	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
+	add	x29, sp, #16
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	mov	w0, #123
+	mov	w1, #456
+	bl	_AddNum
+	stur	w0, [x29, #-4]
+	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
+	add	sp, sp, #32
+	ret
+	.cfi_endproc
+                                        ; -- End function
+.subsections_via_symbols
